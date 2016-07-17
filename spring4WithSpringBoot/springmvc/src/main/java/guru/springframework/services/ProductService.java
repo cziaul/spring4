a513ae24d75b6573d98a2837670bc16017e11e10
Ziaul Chowdhury <cziaul@gmail.com>
2016-07-17 18:33:09 -0500

    Added products and product

---
+++ spring4WithSpringBoot/springmvc/src/main/java/guru/springframework/services/ProductService.java
@@ -0,0 +1,14 @@
+package guru.springframework.services;
+
+import java.util.List;
+
+import guru.springframework.domain.Product;
+
+public interface ProductService {
+	
+	List<Product> listAllProducts();
+	
+	Product getProductById(Integer id);
+	
+
+}
